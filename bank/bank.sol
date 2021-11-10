e7e069de094422d753c47f3853e8bfaa36b266a4
Kara <kara_himanshu@yahoo.com>
2021-11-11 02:03:47 +0530

    Compound Interface

--- bank/bank.sol
+++ bank/bank.sol
@@ -1,8 +1,26 @@
 pragma solidity >=0.7.0 <0.9.0;
 
+
+interface cETH {
+    
+    // define functions of COMPOUND we'll be using
+    
+    function mint() external payable; // to deposit to compound
+    function redeem(uint redeemTokens) external returns (uint); // to withdraw from compound
+    
+    //following 2 functions to determine how much you'll be able to withdraw
+    function exchangeRateStored() external view returns (uint); 
+    function balanceOf(address owner) external view returns (uint256 balance);
+}
+
+
 contract SmartBankAccount {
 
+
     uint totalContractBalance = 0;
+    
+    address COMPOUND_CETH_ADDRESS = 0x859e9d8a4edadfEDb5A2fF311243af80F85A91b8;
+    cETH ceth = cETH(COMPOUND_CETH_ADDRESS);
 
     function getContractBalance() public view returns(uint){
         return totalContractBalance;
@@ -17,13 +35,14 @@ contract SmartBankAccount {
         depositTimestamps[msg.sender] = block.timestamp;
     }
     
-    function getBalance(address userAddress) public view returns(uint) {
-        uint principal = balances[userAddress];
-        uint timeElapsed = block.timestamp - depositTimestamps[userAddress]; //seconds
-        return principal + uint((principal * 7 * timeElapsed) / (100 * 365 * 24 * 60 * 60)) + 1; //simple interest of 0.07%  per year
+    function getBalance(address userAddress) public view returns(uint256) {
+        return ceth.balanceOf(userAddress);
     }
     
     function withdraw() public payable {
+        
+        //CAN YOU OPTIMIZE THIS FUNCTION TO HAVE FEWER LINES OF CODE?
+        
         address payable withdrawTo = payable(msg.sender);
         uint amountToTransfer = getBalance(msg.sender);
         withdrawTo.transfer(amountToTransfer);
